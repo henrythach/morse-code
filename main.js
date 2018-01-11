934eab184e62d2fab1a5701d583e0f2fa8a7e725
Henry Thach <thachhao@gmail.com>
2018-01-11 18:58:00 -0500

    Redesign layout, spacebar binding, down-up timing

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 (function () {
   'use strict'
 
-  var inputElement = document.getElementById('input')
-  inputElement.addEventListener('keydown', onKeyDown)
-  inputElement.addEventListener('keyup', onKeyUp)
+  var SPACEBAR_KEY = 32
+  var startTime = null
+
+  var calibrateDitCount = 0
+  var calibrateDitSum = 0
+  var ditAverage
+
+  var elemDitAverageText = document.getElementById('ditAverageText')
+  var elemDahAverageText = document.getElementById('dahAverageText')
+
+  elemDitAverageText.innerHTML = '_____'
+  elemDahAverageText.innerHTML = '_____'
+
+  document.addEventListener('keydown', onKeyDown)
+  document.addEventListener('keyup', onKeyUp)
 
   function onKeyDown (event) {
-    console.log('keydown')
+    if (event.keyCode !== SPACEBAR_KEY) {
+      return
+    }
 
-    event.preventDefault()
+    if (!startTime) {
+      startTime = Date.now()
+    }
   }
 
   function onKeyUp (event) {
-    console.log('keyup')
+    if (event.keyCode !== SPACEBAR_KEY) {
+      return
+    }
 
-    event.preventDefault()
+    onTick(Date.now() - startTime)
+    startTime = null
+  }
+
+  function onTick (ms) {
+    console.log(new Date(), ms)
+
+    calibrateDitSum += ms
+    calibrateDitCount++
+    ditAverage = calibrateDitSum / calibrateDitCount.toFixed(2)
   }
 })();
